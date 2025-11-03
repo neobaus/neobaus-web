56e4d2dc36601dabd213f0542e0dda4508ae4676
RadySonabu <ardynubanos@yahoo.com>
2025-11-03 14:38:09 +0800

    feat(contact): add Suspense for lazy loading in Contact component
    - Wrapped the Contact component in a Suspense fallback to improve loading performance and user experience.

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -2,13 +2,16 @@
 import { Contact } from "@/components/contact"
 import { Navbar } from "@/components/navbar"
 import { Footer } from "@/components/footer"
+import { Suspense } from "react";
 
 export default function ContactPage() {
   return (
     <div className="min-h-screen">
       <Navbar />
       <main>
-        <Contact />
+        <Suspense fallback={null}>
+          <Contact />
+        </Suspense>
       </main>
       <Footer />
     </div>
